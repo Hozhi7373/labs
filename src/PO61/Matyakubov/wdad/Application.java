9db030260e1f6b1b5c0beb028629a1ec1ddcf6cf
Hozhi7373 <39309979+Hozhi7373@users.noreply.github.com>
2019-01-26 17:39:57 +0400

    Add files via upload

---
+++ src/PO61/Matyakubov/wdad/Application.java
@@ -0,0 +1,7 @@
+package PO61.Matyakubov.wdad;
+
+public class Application {
+    public static void main(String[] args){
+        System.out.println("I'm Matyakubov Akbar, and I'm not a monkey");
+    }
+}
---
+++ src/PO61/Matyakubov/wdad/learn/xml/Genre.java
@@ -0,0 +1,5 @@
+package PO61.Matyakubov.wdad.learn.xml;
+
+public enum Genre {
+    NOVEL, ODE, OPUS, ARTICLE, STORY, PLAY, SKETCH, EPOPEE, EPOS, ESSAY, OTHER
+}
